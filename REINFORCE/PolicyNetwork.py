837bd98a32e86fc9d950de490ef9fcd9f29b2768
Silverbullet <124493967+zsy623@users.noreply.github.com>
2025-08-11 10:13:10 +0800

    Create PolicyNetwork.py

---
+++ REINFORCE/PolicyNetwork.py
@@ -0,0 +1,25 @@
+from torch import nn
+
+class Policy(nn.Module):
+    def __init__(self, state_size, action_size, hidden_size = 128):
+        super(Policy, self).__init__()
+        self.state_size = state_size
+        self.action_size = action_size
+        self.hidden_size = hidden_size
+        self.fc1 = nn.Linear(self.state_size, self.hidden_size)
+        self.fc2 = nn.Linear(self.hidden_size, self.hidden_size)
+        self.fc3 = nn.Linear(self.hidden_size, self.hidden_size)
+        self.fc4 = nn.Linear(self.hidden_size, self.action_size)
+        self.relu = nn.ReLU()
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.relu(self.fc3(x))
+        x = self.fc4(x)
+        x = self.softmax(x)
+        return x
+
+
+
