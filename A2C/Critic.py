eba5cc33a110b4653f0d5e9b9847278f95ca4267
Silverbullet <124493967+zsy623@users.noreply.github.com>
2025-08-12 12:58:28 +0800

    Create Critic.py

---
+++ A2C/Critic.py
@@ -0,0 +1,17 @@
+import torch.nn as nn
+import torch.optim as optim
+
+class Critic(nn.Module):
+    def __init__(self, state_size, hidden_size = 128):
+        super(Critic, self).__init__()
+        self.fc1 = nn.Linear(state_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, 1)
+        self.relu = nn.ReLU()
+        self.optimizer = optim.Adam(self.parameters(), lr = 1e-3)
+
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
